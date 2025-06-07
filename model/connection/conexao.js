3033d5ef47603f3f11aa56c873d201393163cac6
James Pereira dos Santos Amarante <92009914+JamesAmarante@users.noreply.github.com>
2025-06-07 12:10:16 -0300

    Update conexao.js

--- model/connection/conexao.js
+++ model/connection/conexao.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(`${process.env.DB}`);
+const sequelize = new Sequelize(`${process.env.DB}`, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {},
+});
 
 module.exports = sequelize
